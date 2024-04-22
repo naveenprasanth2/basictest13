99544116f7df433c25a8add90717bce104f63a06
naveenprasanth <developer.hotel@bookany.in>
2024-04-22 19:22:40 +0530

    cleared sonar issues

--- src/main/java/org/example/practise1/OptionalTest.java
+++ src/main/java/org/example/practise1/OptionalTest.java
@@ -6,21 +6,24 @@ import lombok.extern.log4j.Log4j2;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.function.ToIntFunction;
 import java.util.stream.IntStream;
 
 @Log4j2
 public class OptionalTest {
+
+    private static final String WITHOUT_ARGUMENTS = "without arguments";
+
     public static void main(String[] args) {
         OptionalTest optionalTest = new OptionalTest();
         String value = Optional.of("test").orElseThrow(NoSuchElementException::new);
         log.info(value);
 
-//        Parent parent = new Child(5);
-//        log.info(parent.number);
+        Parent parent = new Child(5);
+        log.info(parent.number);
         IntStream.rangeClosed(1, 10).forEach(optionalTest::print);
-        Function<String, Integer> function = String::length;
+        ToIntFunction<String> function = String::length;
         BiFunction<String, String, Integer> function1 = (String name, String city) -> (name + city).length();
         log.info(optionalTest.test(function));
         log.info(optionalTest.test1(function1));
@@ -34,11 +37,11 @@ public class OptionalTest {
     }
 
     private String printWithoutArgs() {
-        return "without arguments";
+        return WITHOUT_ARGUMENTS;
     }
 
-    private Integer test(Function<String, Integer> function) {
-        return function.apply("testing");
+    private Integer test(ToIntFunction<String> function) {
+        return function.applyAsInt("testing");
     }
 
     private Integer test1(BiFunction<String, String, Integer> function1) {
