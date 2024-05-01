6341a89d8c07edecb38d3386ef0a73738d9a32d4
naveenprasanth <developer.hotel@bookany.in>
2024-05-01 20:30:59 +0530

    Grade Test code

--- src/test/java/org/example/unit/GradeTest.java
+++ src/test/java/org/example/unit/GradeTest.java
@@ -6,9 +6,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GradeTest {
     @Test
-    void checkGrade(){
+    void fiftyNineShouldReturnF() {
         Grade grade = new Grade();
-        System.out.println(grade.determineGrade(1));
+        assertEquals('F', grade.determineGrade(59));
+    }
+
+    @Test
+    void sixtyNineShouldReturnD() {
+        Grade grade = new Grade();
+        assertEquals('D', grade.determineGrade(69));
+    }
+
+    @Test
+    void seventyNineShouldReturnC() {
+        Grade grade = new Grade();
+        assertEquals('C', grade.determineGrade(79));
+    }
+
+    @Test
+    void eightyNineShouldReturnB() {
+        Grade grade = new Grade();
+        assertEquals('B', grade.determineGrade(89));
+    }
+
+    @Test
+    void ninetyNineShouldReturnA() {
+        Grade grade = new Grade();
+        assertEquals('A', grade.determineGrade(99));
+    }
+
+    @Test
+    void oneHundredOneShouldThrowIllegalArgumentException() {
+        Grade grade = new Grade();
+        assertThrows(IllegalArgumentException.class, () -> grade.determineGrade(101));
+    }
+
+    @Test
+    void lessThanZeroShouldThrowIllegalArgumentException() {
+        Grade grade = new Grade();
+        assertThrows(IllegalArgumentException.class, () -> grade.determineGrade(-1));
     }
 
 }
