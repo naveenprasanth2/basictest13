01790b7549b2920981ec103aea94bf3b67283f0c
naveen prasanth <naveenprasanth12@gmail.com>
2024-05-04 18:19:39 +0530

    bubble sort logic

---
+++ src/main/java/org/example/sorting/BubbleSort.java
@@ -0,0 +1,21 @@
+package org.example.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] a = {4, 3, 2, 4, 5, 6, 3, 1};
+
+        for (int i = 0; i< a.length; i++){
+            for (int j = 0; j<a.length -1 ; j++){
+                if(a[j] > a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(a));
+    }
+}
