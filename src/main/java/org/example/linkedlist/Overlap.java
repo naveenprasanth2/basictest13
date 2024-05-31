cebaff5f921309d9f32e45a0ed869a6f1b6136d0
naveen prasanth <naveenprasanth12@gmail.com>
2024-05-31 17:59:30 +0530

    overlap changes

--- src/main/java/org/example/linkedlist/Overlap.java
+++ src/main/java/org/example/linkedlist/Overlap.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-public class OverlapExample {
+public class Overlap {
     public static void main(String[] args) {
         List<Integer> scheduleStart = Arrays.asList(1, 1, 2);
         List<Integer> scheduleEnd = Arrays.asList(3, 2, 4);
         int size = scheduleStart.size();
         int[][] events = new int[size][2];
 
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++){
             events[i][0] = scheduleStart.get(i);
             events[i][1] = scheduleEnd.get(i);
         }
@@ -21,8 +21,8 @@ public class OverlapExample {
         int count = 0;
         int lastEndTime = 0;
 
-        for (int i = 0; i < size; i++) {
-            if (events[i][0] >= lastEndTime) {
+        for (int i = 0; i < size; i++){
+            if(events[i][0] >= lastEndTime){
                 count++;
                 lastEndTime = events[i][1];
             }
