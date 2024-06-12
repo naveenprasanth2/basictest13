4e61697d0c85bf600b9a2b46b89a5c5f8145beae
naveen prasanth <naveenprasanth12@gmail.com>
2024-06-12 14:20:45 +0530

    merge sorted arrays

--- src/main/java/org/example/challenge/MergeSortedArrays.java
+++ src/main/java/org/example/challenge/MergeSortedArrays.java
@@ -1,9 +1,27 @@
 package org.example.challenge;
 
+import java.util.Arrays;
+
 public class MergeSortedArrays {
     public static void main(String[] args) {
-        int[] nums1 = {1, 2, 0, 0};
-        int[] nums2 = {3, 4};
-        int length = 2;
+        int[] a = {1, 2, 4, 0, 0, 0, 0, 0, 0};
+        int[] b = {2, 3, 4, 5, 6, 7};
+
+        int aSize = 2;
+        int bSize = 4;
+
+        int length = a.length - 1;
+
+        while (aSize >= 0 && bSize >= 0){
+            if(a[aSize] < b[bSize]){
+                a[length--] = b[bSize--];
+            }else {
+                a[length--] = a[aSize--];
+            }
+        }
+
+
+
+        System.out.println(Arrays.toString(a));
     }
 }
